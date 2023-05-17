e03201ad1b42200d3d7ac2c1069826edde507ad5
Tinoet <elijahtinoet@gmail.com>
2023-05-17 13:51:31 +0100

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# react-class
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/react-z9shms)
---
+++ src/App.js
@@ -0,0 +1,65 @@
+import React from "react";
+import "./style.css";
+
+// function MyButton() {
+//   return (
+//     <button>
+//       I'm a button
+      
+//     </button>
+    
+//   );
+// }
+
+// export default function MyApp() {
+//   return (
+//     <div>
+//       <h1>Welcome to my app</h1>
+//       <MyButton />
+//     </div>
+//   );
+// }
+const user = {
+  name: 'Hedy Lamarr',
+  imageUrl: 'https://i.imgur.com/yXOvdOSs.jpg',
+  imageSize: 90,
+};
+
+export default function Profile() {
+  return (
+    <>
+      <h1>{user.name}</h1>
+      <img
+        className="avatar"
+        src={user.imageUrl}
+        alt={'Photo of ' + user.name}
+        style={{
+          width: user.imageSize,
+          height: user.imageSize
+        }}
+      />
+    </>
+  );
+}
+const products = [
+  { title: 'Cabbage', isFruit: false, id: 1 },
+  { title: 'Garlic', isFruit: false, id: 2 },
+  { title: 'Apple', isFruit: true, id: 3 },
+];
+
+export default function ShoppingList() {
+  const listItems = products.map(product =>
+    <li
+      key={product.id}
+      style={{
+        color: product.isFruit ? 'magenta' : 'darkgreen'
+      }}
+    >
+      {product.title}
+    </li>
+  );
+
+  return (
+    <ul>{listItems}</ul>
+  );
+}
